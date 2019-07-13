1cad70bc70e892fdbb6ec8eabb8e9cb213573a8a
zori0 <776021342@qq.com>
2019-07-13 19:47:20 +0800

    修改

--- user/user-service/src/main/java/com/dzz/user/service/domain/dao/UserMapper.java
+++ user/user-service/src/main/java/com/dzz/user/service/domain/dao/UserMapper.java
@@ -3,6 +3,5 @@ package com.dzz.user.service.domain.dao;
 import com.dzz.user.service.domain.model.User;
 import tk.mybatis.mapper.common.Mapper;
 
-
 public interface UserMapper extends Mapper<User> {
 }
--- user/user-service/src/main/java/com/dzz/user/service/domain/model/User.java
+++ user/user-service/src/main/java/com/dzz/user/service/domain/model/User.java
@@ -1,242 +1,72 @@
 package com.dzz.user.service.domain.model;
 
-import java.util.Date;
 import javax.persistence.*;
 
 public class User {
-    /**
-     * 自增ID
-     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    /**
-     * 用户Id
-     */
-    @Column(name = "user_id")
-    private String userId;
-
-    /**
-     * 用户名
-     */
     @Column(name = "user_name")
     private String userName;
 
-    /**
-     * 密码
-     */
     private String password;
 
-    /**
-     * 手机号
-     */
-    private String mobile;
+    private String role;
 
     /**
-     * 邮箱
-     */
-    private String email;
-
-    /**
-     * 联系人
-     */
-    @Column(name = "contact_person")
-    private String contactPerson;
-
-    /**
-     * 联系人邮箱
-     */
-    @Column(name = "contact_email")
-    private String contactEmail;
-
-    /**
-     * 创建时间
-     */
-    @Column(name = "create_time")
-    private Date createTime;
-
-    /**
-     * 修改时间
-     */
-    @Column(name = "update_time")
-    private Date updateTime;
-
-    /**
-     * 获取自增ID
-     *
-     * @return id - 自增ID
+     * @return id
      */
     public Integer getId() {
         return id;
     }
 
     /**
-     * 设置自增ID
-     *
-     * @param id 自增ID
+     * @param id
      */
     public void setId(Integer id) {
         this.id = id;
     }
 
     /**
-     * 获取用户Id
-     *
-     * @return user_id - 用户Id
-     */
-    public String getUserId() {
-        return userId;
-    }
-
-    /**
-     * 设置用户Id
-     *
-     * @param userId 用户Id
-     */
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    /**
-     * 获取用户名
-     *
-     * @return user_name - 用户名
+     * @return user_name
      */
     public String getUserName() {
         return userName;
     }
 
     /**
-     * 设置用户名
-     *
-     * @param userName 用户名
+     * @param userName
      */
     public void setUserName(String userName) {
         this.userName = userName;
     }
 
     /**
-     * 获取密码
-     *
-     * @return password - 密码
+     * @return password
      */
     public String getPassword() {
         return password;
     }
 
     /**
-     * 设置密码
-     *
-     * @param password 密码
+     * @param password
      */
     public void setPassword(String password) {
         this.password = password;
     }
 
     /**
-     * 获取手机号
-     *
-     * @return mobile - 手机号
+     * @return role
      */
-    public String getMobile() {
-        return mobile;
+    public String getRole() {
+        return role;
     }
 
     /**
-     * 设置手机号
-     *
-     * @param mobile 手机号
+     * @param role
      */
-    public void setMobile(String mobile) {
-        this.mobile = mobile;
-    }
-
-    /**
-     * 获取邮箱
-     *
-     * @return email - 邮箱
-     */
-    public String getEmail() {
-        return email;
-    }
-
-    /**
-     * 设置邮箱
-     *
-     * @param email 邮箱
-     */
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    /**
-     * 获取联系人
-     *
-     * @return contact_person - 联系人
-     */
-    public String getContactPerson() {
-        return contactPerson;
-    }
-
-    /**
-     * 设置联系人
-     *
-     * @param contactPerson 联系人
-     */
-    public void setContactPerson(String contactPerson) {
-        this.contactPerson = contactPerson;
-    }
-
-    /**
-     * 获取联系人邮箱
-     *
-     * @return contact_email - 联系人邮箱
-     */
-    public String getContactEmail() {
-        return contactEmail;
-    }
-
-    /**
-     * 设置联系人邮箱
-     *
-     * @param contactEmail 联系人邮箱
-     */
-    public void setContactEmail(String contactEmail) {
-        this.contactEmail = contactEmail;
-    }
-
-    /**
-     * 获取创建时间
-     *
-     * @return create_time - 创建时间
-     */
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    /**
-     * 设置创建时间
-     *
-     * @param createTime 创建时间
-     */
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    /**
-     * 获取修改时间
-     *
-     * @return update_time - 修改时间
-     */
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    /**
-     * 设置修改时间
-     *
-     * @param updateTime 修改时间
-     */
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
+    public void setRole(String role) {
+        this.role = role;
     }
 }
