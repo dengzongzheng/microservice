c9efd98cb9fdc604e93363223cf22a79cca6447d
zori0 <776021342@qq.com>
2019-08-09 18:05:22 +0800

    修改

---
+++ gateway/src/main/java/com/dzz/gateway/cors/CorsFilter.java
@@ -0,0 +1,43 @@
+package com.dzz.gateway.cors;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+/**
+ * 跨域请求
+ *
+ * @author dzz
+ * @version 1.0.0
+ * @since 2019年08月09 16:56
+ */
+@Configuration
+@EnableWebFlux
+public class CorsFilter implements WebFluxConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(true)
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("*");
+    }
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedOrigin("*");
+        UrlBasedCorsConfigurationSource corsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        corsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsWebFilter(corsConfigurationSource);
+    }
+}
--- gateway/src/main/java/com/dzz/gateway/routers/Router.java
+++ gateway/src/main/java/com/dzz/gateway/routers/Router.java
@@ -2,9 +2,12 @@ package com.dzz.gateway.routers;
 
 import com.dzz.gateway.filter.TimingFilter;
 import com.dzz.gateway.limiter.HostAddrKeyResolver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -17,14 +20,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Router {
 
+    @Autowired
+    private ReactiveRedisTemplate<String,String> redisTemplate;
+
+
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("policy_service_route", r -> r.path("/api/hello/**")
                         .filters(f -> f.hystrix(
                                 config -> config.setName("mycmd").setFallbackUri("forward:/fallback")))
-//                        .uri("lb://POLICY-SERVICE")
-                        .uri("http://127.0.0.1:7002")
+//                        .filters(f -> f.requestRateLimiter()
+//                                .configure(c -> c.setRateLimiter(getRedisRateLimiter())))
+                        .uri("lb://POLICY-SERVICE")
                         .filter(new TimingFilter())
                         .order(1)
                         .id("policy-service-router"))
@@ -35,5 +43,11 @@ public class Router {
     @Bean
     public HostAddrKeyResolver hostAddrKeyResolver() {
         return new HostAddrKeyResolver();
+    }
+
+    @Bean
+    public RedisRateLimiter getRedisRateLimiter() {
+
+        return new RedisRateLimiter(100,2);
     }
 }
